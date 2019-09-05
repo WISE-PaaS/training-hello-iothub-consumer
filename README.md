124cb53e958a8ecdfc1229972093adad54bc250a
Dave Lee <isho.lee@gmail.com>
2019-09-05 11:11:57 +0800

    Changed manifest settings and modified README.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# training-hello-iothub-consumer" 
+# training-hello-iothub-consumer
+
+A simple example app that connects to the WISE-PaaS IoT-Hub, subscribes to "all" topics, and consumes the messages which are published to any topic on the IoT-Hub.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mqtt      = require('mqtt');      // Using MQTT.js npm
+const mqtt = require('mqtt');      // Using MQTT.js npm
 const app = express();
 
 app.get('/', (req, res) => {
@@ -11,17 +11,17 @@ const port = process.env.PORT || 3030;
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // Start Config
-var config    = {};
-config.mqtt   = {};
+var config = {};
+config.mqtt = {};
 
 /** Modify this config ***/
 // SYS
-config.timeout          = 120 * 1000;
+config.timeout = 120 * 1000;
 // Local environment MQTT configuration
-config.mqtt.broker      = "MODIFY_BROKER_HOST";     // mqtt://broker-host
-config.mqtt.username    = "MODIFY_YOUR_USERNAME";
-config.mqtt.port        = 1883;
-config.mqtt.password    = "MODIFY_YOUR_PASSWORD";
+config.mqtt.broker = "MODIFY_BROKER_HOST";     // mqtt://broker-host
+config.mqtt.username = "MODIFY_YOUR_USERNAME";
+config.mqtt.port = 1883;
+config.mqtt.password = "MODIFY_YOUR_PASSWORD";
 /*****/
 
 config.mqtt.serviceName = "rabbitmq"; // 'p-rabbitmq'
@@ -30,7 +30,7 @@ var vcap_services = {
   "p-rabbitmq": [
     {
       "credentials": {
-        "protocols": {  
+        "protocols": {
           "mqtt": {
             "username": config.mqtt.username,
             "password": config.mqtt.password,
@@ -44,52 +44,52 @@ var vcap_services = {
 };
 
 if (process.env.VCAP_SERVICES != null) {
-    console.log("Using VCAP_SERVICES");
-    vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+  console.log("Using VCAP_SERVICES");
+  vcap_services = JSON.parse(process.env.VCAP_SERVICES);
 }
 
 // Parsing credentials from VCAP_SERVICES for binding service
-if(vcap_services[config.mqtt.serviceName]){
-    console.log("Parsing "+config.mqtt.serviceName);
-    config.mqtt.broker		= "mqtt://" + vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.host;
-    config.mqtt.username	= vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.username.trim();
-    config.mqtt.password	= vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.password.trim();
-    config.mqtt.port		= vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.port;
+if (vcap_services[config.mqtt.serviceName]) {
+  console.log("Parsing " + config.mqtt.serviceName);
+  config.mqtt.broker = "mqtt://" + vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.host;
+  config.mqtt.username = vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.username.trim();
+  config.mqtt.password = vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.password.trim();
+  config.mqtt.port = vcap_services[config.mqtt.serviceName][0].credentials.protocols.mqtt.port;
 }
 
 config.mqtt.options = {
-	broker: config.mqtt.broker,
-	reconnectPeriod: 1000,
-	port: config.mqtt.port,
-	username: config.mqtt.username,
-	password: config.mqtt.password
+  broker: config.mqtt.broker,
+  reconnectPeriod: 1000,
+  port: config.mqtt.port,
+  username: config.mqtt.username,
+  password: config.mqtt.password
 };
 
 console.log(config.mqtt.options);
 
-config.mqtt.topic		= "/#";
-config.mqtt.retain		= true; // MQTT Publish Retain
+config.mqtt.topic = "#";
+config.mqtt.retain = true; // MQTT Publish Retain
 
 // Start MQTT
-var client    = mqtt.connect(config.mqtt.broker,config.mqtt.options);
+var client = mqtt.connect(config.mqtt.broker, config.mqtt.options);
 
 client.on('connect', function () {
-    client.subscribe(config.mqtt.topic);
-    console.log("[MQTT]:", "Connected.");
+  client.subscribe(config.mqtt.topic);
+  console.log("[MQTT]:", "Connected.");
 });
- 
+
 client.on('message', function (topic, message) {
-    console.log("[" + topic + "]:" + message.toString());
+  console.log("[" + topic + "]:" + message.toString());
 });
 
-client.on('error', function(err) {
-    console.log(err);
+client.on('error', function (err) {
+  console.log(err);
 });
 
-client.on('close', function() {
-    console.log("[MQTT]: close");
+client.on('close', function () {
+  console.log("[MQTT]: close");
 });
 
-client.on('offline', function() {
-    console.log("[MQTT]: offline");
+client.on('offline', function () {
+  console.log("[MQTT]: offline");
 });
